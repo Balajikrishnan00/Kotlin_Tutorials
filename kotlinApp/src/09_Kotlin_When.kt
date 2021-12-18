289ed71ae74c7daa6fe0931857f7019c61cb83f0
balaji <balajikrishnan00@gmail.com>
2021-12-18 22:43:02 +0530

    "commit"

---
+++ kotlinApp/src/09_Kotlin_When.kt
@@ -0,0 +1,40 @@
+fun main() {
+    /*
+    var number= readLine()
+
+    var day=when(number!!.toInt()){
+        1->"Sunday"
+        2->"Monday"
+        3->"Tuesday"
+        4->"Wednesday"
+        5->"Thursday"
+        6->"Friday"
+        7->"Saturday"
+        else->"invalid number"
+    }
+    println(day)
+    ---------------------------------------------------
+    print("Enter a Student Register Number :")
+    val regNumber= readLine()
+
+    when(regNumber){
+        "112"->println("Hi Balaji")
+        "113"->println("Hi Shiva")
+        "114"->println("Hi Selvam")
+        else->println("Invalid Number Pls ReEnter")
+
+    }
+    ---------------------------------------------------
+
+
+    // Group Find
+    println("Enter a Team or Team number:")
+    val Team= readLine()
+    when ((Team!!.toInt()) ||  (Team!!.toString())){
+        1,2,3,4,5->println("Team A")
+
+    }
+
+     */
+
+}
